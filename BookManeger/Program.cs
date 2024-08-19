28f2cb224e2f3240b4319de07fc32b66410b2ac9
Eliton Brighenti <elitonbrighenti90@gmail.com>
2024-08-19 19:51:08 -0300

    PRIMEIRO COMMIT

--- BookManeger/Program.cs
+++ BookManeger/Program.cs
@@ -4,18 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Livro livro1 = new Livro("O alquimista", "Paulo Celho", 1998, 5);
+            Livro livro1 = new Livro("TESTE GIT", "Paulo Celho", 1998, 5);
             Livro livro2 = new Livro("Banana", "Aleatorio", 2005, 10);
 
             Biblioteca biblioteca01 = new Biblioteca();
 
             biblioteca01.AdicionarLibro(livro1);
             biblioteca01.AdicionarLibro(livro2);
-
-
-
-
-
 
             DateTime dataDevolucao = new DateTime(2024, 8, 15);
         }
